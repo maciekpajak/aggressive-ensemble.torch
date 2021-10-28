a7b526bdfe85164ff94415cdb87de05314e31fe0
Maciej Pająk <mpajak98@gmail.com>
2021-10-28 13:13:30 +0200

    Combine answers function optimized

--- aggressive_ensemble/Ensemble.py
+++ aggressive_ensemble/Ensemble.py
@@ -37,21 +37,25 @@ def combine_answers_to_rankings(answers: List[pd.DataFrame]) -> pd.DataFrame:
         raise ValueError("Answers list cannot be empty")
 
     tags = answers[0].iloc[:, 0].values
-    tmp = answers[0].copy(deep=False).set_index(tags)
+    columns = answers[0].columns.values
 
-    for col in tmp.columns.values:
-        for tag in tags:
-            rank = 0.0
-            for ans in answers:
-                rank += ans[ans[col] == tag].index.values[0]
-            tmp.loc[tag, col] = rank / len(answers)
+    dic = {col: pd.DataFrame(columns=tags, index=range(0, len(answers))) for col in columns}
 
-    rpreds = tmp.copy(deep=False)
-    for col in tmp.columns.values:
-        rpreds.loc[:, col] = tmp.sort_values(by=col, ascending=True).index
-    rpreds = rpreds.reset_index(drop=True)
+    for ans, ans_idx in zip(answers, range(0, len(answers))):
+        for col in ans:
+            rank = 1
+            for val in ans[col]:
+                dic[col][val][ans_idx] = rank
+                rank += 1
 
-    return rpreds
+    answer = pd.DataFrame(columns=columns, index=tags)
+    for d in dic:
+        answer[d] = dic[d].mean()
+    for col in columns:
+        answer[col] = answer.sort_values(by=col, ascending=True).index
+    answer = answer.reset_index(drop=True)
+
+    return answer
 
 
 def combine_answers_to_probabilities(answers: List[pd.DataFrame]) -> pd.DataFrame:
